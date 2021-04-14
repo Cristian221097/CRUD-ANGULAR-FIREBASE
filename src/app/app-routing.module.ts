a61bd504713de624d2aa647b61b6567319e61085
Cristian Amancio <cristianjoelamancioherrera@gmail.com>
2021-04-14 10:16:22 +0800

    instalando bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', loadChildren: () => import('./components/create/create.module').then(m => m.CreateModule) }, 
+  { path: 'list', loadChildren: () => import('./components/list/list.module').then(m => m.ListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
